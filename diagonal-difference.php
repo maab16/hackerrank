b94e6035a30f217c97720e14ef17048cf5aa276b
Md Abu Ahsan Basir <maab.career@gmail.com>
2017-06-26 05:57:13 +0600

    Update Diagonal Difference

--- diagonal-difference.php
+++ diagonal-difference.php
@@ -1,4 +1,5 @@
 <?php
+// Option One
 /*
  * Problem Link : https://www.hackerrank.com/challenges/diagonal-difference/
  * Author : Md Abu Ahsan Basir
@@ -40,5 +41,50 @@ function getAntiDiagonalValue($n,array $matrix){
 }
 
 echo abs(array_sum(getDiagonalValue($n,$a)) - array_sum(getAntiDiagonalValue($n,$a)));
+
+?>
+
+<?php
+// Option Two
+/*
+ * Problem Link : https://www.hackerrank.com/challenges/diagonal-difference/
+ * Author : Md Abu Ahsan Basir
+ * Personal : www.code-press.com/resume
+ * Hackerrank : https://www.hackerrank.com/abuahsan
+ * GitHub : https://github.com/maab16/
+ * Gitlab : https://gitlab.com/maab16 
+ * Stackoverflow: https://stackoverflow.com/users/6677064/md-abu-ahsan-basir 
+ */
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d",$n);
+$a = array();
+for($a_i = 0; $a_i < $n; $a_i++) {
+   $a_temp = fgets($handle);
+   $a[] = explode(" ",$a_temp);
+  array_walk($a[$a_i],'intval');
+}
+//Main Code Started Here
+$n = count($a);
+
+function getDiagonalValue($n,array $matrix){
+    $values = 0;
+    for($i = 0;$i < $n;$i++)
+    {
+           $values += $matrix[$i][$i];
+    }
+    return $values;
+}
+
+function getAntiDiagonalValue($n,array $matrix){
+    $values = 0;
+    for($i=0;$i<$n;$i++)
+    {
+        $values += $matrix[$i][$n-1-$i];
+        
+    }
+    return $values;
+}
+
+echo abs(getDiagonalValue($n,$a) - getAntiDiagonalValue($n,$a));
 
 ?>
